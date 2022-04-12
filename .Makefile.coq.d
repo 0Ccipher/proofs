6146882391ef0ba702a1c42167fcaa5f82889a8e
Ashutosh Gupta <akg@iitb.ac.in>
2022-04-12 15:31:20 +0530

    dsadsa

--- .Makefile.coq.d
+++
@@ -1,9 +0,0 @@
-util.vo util.glob util.v.beautified util.required_vo: util.v 
-util.vio: util.v 
-util.vos util.vok util.required_vos: util.v 
-tmm.vo tmm.glob tmm.v.beautified tmm.required_vo: tmm.v util.vo
-tmm.vio: tmm.v util.vio
-tmm.vos tmm.vok tmm.required_vos: tmm.v util.vos
-ccv.vo ccv.glob ccv.v.beautified ccv.required_vo: ccv.v util.vo
-ccv.vio: ccv.v util.vio
-ccv.vos ccv.vok ccv.required_vos: ccv.v util.vos
--- ccv.v
+++ ccv.v
@@ -9,15 +9,18 @@ Require Import ZArith.
 Require Import Ensembles.
 Require Import TMM.util.
 
+Set Implicit Arguments.
 
-(* Inductive Tactions : Set := *)
-(*   | begint : Tactions *)
-(*   | endt : Tactions *)
-(*   | delt : Tactions. *)
-(* Lemma eqTactions_dec : forall (x y: Tactions), {x=y} + {x <> y}. *)
-(* Proof. *)
-(* decide_equality. *)
-(* Defined. *)
+Ltac decide_equality := decide equality; auto with equality arith.
+
+Inductive Tactions : Set :=
+  | begint : Tactions
+  | endt : Tactions
+  | delt : Tactions.
+Lemma eqTactions_dec : forall (x y: Tactions), {x=y} + {x <> y}.
+Proof.
+decide_equality.
+Defined.
 (* Global Hint Resolve eqTactions_dec : equality. *)
 
 (* Check Tactions. *)
